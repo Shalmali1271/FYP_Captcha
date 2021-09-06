ebda9e5341c2c2436649c8da989800eb5227d7fd
Shalmali1271 <59409625+Shalmali1271@users.noreply.github.com>
2021-09-06 10:26:23 +0530

    Image Captcha

--- frontend/settings.py
+++ frontend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_w8*^1pip%e7%03!e*i9o3p@^q4*%wcvfm_*pf@2b-tztuh5#+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fyp-image-captcha.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
